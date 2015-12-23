93f77a8e5b1e0b5f38a6f0307c75724f9331e81f
Jamie Nicol <jamie@thenicols.net>
2015-12-23 08:31:01 +0000

    Initial commit
    Launcher icon opens invisible activity which immediately closes itself.
    
    ¯\_(ツ)_/¯

---
+++ src/main/java/org/jamienicol/shrug/MainActivity.java
@@ -0,0 +1,17 @@
+package org.jamienicol.shrug;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+
+public class MainActivity
+    extends Activity
+{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        finish();
+    }
+}
