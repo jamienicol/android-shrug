062dec2d0312fb620bc5d347b9195a9d54834a1a
Jamie Nicol <jamie@thenicols.net>
2015-12-23 10:31:22 +0000

    Show shrug toast when launched

--- src/main/java/org/jamienicol/shrug/MainActivity.java
+++ src/main/java/org/jamienicol/shrug/MainActivity.java
@@ -3,6 +3,7 @@ package org.jamienicol.shrug;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity
     extends Activity
@@ -12,6 +13,16 @@ public class MainActivity
     {
         super.onCreate(savedInstanceState);
 
+        shrug();
+
         finish();
+    }
+
+    private void shrug() {
+        // Show a toast to notify the user
+        final Toast toast = Toast.makeText(this,
+                                           getString(R.string.toast_text),
+                                           Toast.LENGTH_SHORT);
+        toast.show();
     }
 }
