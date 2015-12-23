6b8a3c4427bfd9cf5e7acebeeb401f83af076d6d
Jamie Nicol <jamie@thenicols.net>
2015-12-23 10:46:59 +0000

    Copy shrug to clipboard when launched

--- src/main/java/org/jamienicol/shrug/MainActivity.java
+++ src/main/java/org/jamienicol/shrug/MainActivity.java
@@ -1,6 +1,8 @@
 package org.jamienicol.shrug;
 
 import android.app.Activity;
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.content.Context;
 import android.os.Bundle;
 import android.widget.Toast;
@@ -19,6 +21,14 @@ public class MainActivity
     }
 
     private void shrug() {
+        // Copy to clipboard
+        final ClipboardManager clipboard =
+            (ClipboardManager)getSystemService(Context.CLIPBOARD_SERVICE);
+        final ClipData clip =
+            ClipData.newPlainText(getString(R.string.clipboard_label),
+                                  getString(R.string.clipboard_text));
+        clipboard.setPrimaryClip(clip);
+
         // Show a toast to notify the user
         final Toast toast = Toast.makeText(this,
                                            getString(R.string.toast_text),
